479c3a32b5276e5d645b7067cce3a250e5d204ab
gobeumsu <gobeumsu@gmail.com>
2025-05-09 12:36:40 +0100

    refactor : Move behaviours close to data (account)

--- Account.py
+++ Account.py
@@ -17,8 +17,17 @@ class Account:
     def set_balance(self, balance):
         self.balance = balance
 
+    def add_balance(self, amount):
+        self.balance += amount
+
     def get_interest_rate(self):
         return self.interest_rate
 
     def set_interest_rate(self, rate):
         self.interest_rate = rate
+    def add_interest(self):
+        self.balance = self.get_balance() +self.get_balance() * self.get_interest_rate()
+
+    def close_account(self):
+        self.set_customer(None)
+        self.set_balance(0)
