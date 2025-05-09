de16931535e5c6382dbd5dc07dfeb14237dc9696
gobeumsu <gobeumsu@gmail.com>
2025-05-09 12:42:17 +0100

    refactor : Move behaviours close to data (customer)

--- Customer.py
+++ Customer.py
@@ -1,6 +1,8 @@
 class Customer:
-    def __init__(self, name: str):
+    def __init__(self, name: str,address:str = "NO ADDRESS", phone_number:str = "NO PHONE NUMBER"):
         self._name = name
+        self.address = address
+        self.phone_number = phone_number
 
     def set_name(self, name: str):
         self._name = name
@@ -8,3 +10,12 @@ class Customer:
     def get_name(self):
         return self._name
 
+    def set_address(self, address: str):
+        self.address = address
+    def get_address(self):
+        return self.address
+
+    def set_phone_number(self, phone_number: str):
+        self.phone_number = phone_number
+    def get_phone_number(self):
+        return self.phone_number
